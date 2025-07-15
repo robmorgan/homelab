bf8d23adcb000a13f55da3b1b12b3cdee4406070
Rob Morgan <robbym@gmail.com>
2025-07-15 14:13:44 +0800

    fix: allow sonos connectivity

--- bin/install-cert-manager.sh
+++ bin/install-cert-manager.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --set crds.enabled=true
